2305b6bf90b2ab0b30070803a7b304ef31098377
emmadesuyrot <desue472@newschool.edu>
2025-04-08 21:46:34 -0400

    JS Learnings and Source

--- main.js
+++ main.js
@@ -12,8 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
 				const price = document.getElementById('price').value;
 				const benchmarkSelect = document.getElementById('benchmark');
 				const benchmark = parseFloat(benchmarkSelect.value);
+				// parseFloat() to converts string input into a number
+				// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat
 				const selectedOption = benchmarkSelect.options[benchmarkSelect.selectedIndex];
 				const icon = selectedOption.getAttribute('data-icon');
+				// getAttribute to retrieve values from HTML attributes
+				// Source: https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttribute
 				const itemName = selectedOption.textContent;
 				const resultBox = document.getElementById('result-box');
 				const itemTitle = selectedOption.getAttribute('title');
@@ -25,6 +29,8 @@ document.addEventListener('DOMContentLoaded', () => {
 				errorMessage.textContent = 'Please enter a valid price.';
 				return;
  			}
+			// isNaN(price) to check if a value is not a number
+			// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
 
 			// If modal is the better option for the error state (below)
 			// if (!price || isNaN(price) || price <= 0) {
@@ -49,7 +55,10 @@ document.addEventListener('DOMContentLoaded', () => {
 			const quantity = Math.floor(price / benchmark);
 
 			let iconsDisplay = quantity <= 10 ? icon.repeat(quantity) : ''; 
+			// to repeat a string (icon) based on a number
+			// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 			let resultText = `You can buy <strong>${quantity}</strong> ${icon} ${itemName}.`;
+			// ${} to insert them into a sentence
 			if (itemTitle) {
 				resultText += `<br><em class="item-title">(${itemTitle})</em>`;
 			}
@@ -62,6 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		resultModal.showModal();
 	};
 		// Used template literals (backticks `) for string interpolation
+		// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
 		closeButtons.forEach(button => {
 			button.onclick = () => {
